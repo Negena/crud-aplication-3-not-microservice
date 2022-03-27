f91fb971fa60c73a4d05cc7b4e670dbb61451209
Negena Veid <75137271+Negena@users.noreply.github.com>
2022-03-27 20:53:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb+srv://user1:useronce@cluster0.txaa5.mongodb.net/getData", {
+mongoose.connect(proccess.env.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
